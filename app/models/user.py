efe8c8d38d5e1ef24b5de563eba50fe70ed1d83c
nik blago <159848859+oryx3422@users.noreply.github.com>
2025-05-12 22:54:33 +0300

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import DeclarativeBase
 class Base(DeclarativeBase):
     pass
 
-class UserModel(Base):  # Модель пользователя в БД
+class UserModel(Base):  
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
